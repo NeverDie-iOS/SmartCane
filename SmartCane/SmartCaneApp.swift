f0ec6842b83a783492a7b1e98eb411c3329b7930
hyunsoo <mm083434@naver.com>
2025-10-01 16:54:08 +0900

    Initial Commit

---
+++ SmartCane/SmartCaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCaneApp.swift
+//  SmartCane
+//
+//  Created by 현수 on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
