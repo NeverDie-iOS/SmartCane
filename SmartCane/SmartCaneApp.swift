a9dfb3e6c575bfa85c38d1f98f93b1764ed33243
hyunsoo <mm083434@naver.com>
2025-10-01 19:46:18 +0900

    생성 - MainView(가명)

--- SmartCane/SmartCaneApp.swift
+++ SmartCane/SmartCaneApp.swift
@@ -1,17 +1,10 @@
-//
-//  SmartCaneApp.swift
-//  SmartCane
-//
-//  Created by 현수 on 10/1/25.
-//
-
 import SwiftUI
 
 @main
 struct SmartCaneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
