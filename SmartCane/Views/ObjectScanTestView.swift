8a4496b24c1e82aaa482fc67e051afd18b3c962d
hyunsoo <mm083434@naver.com>
2025-10-07 23:21:00 +0900

    ObjectScanTestView 앨범에서 사진 가져오기 구현

--- SmartCane/Views/ObjectScanTestView.swift
+++ SmartCane/Views/ObjectScanTestView.swift
@@ -1,8 +1,45 @@
 import SwiftUI
+import PhotosUI
 
 struct ObjectScanTestView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedItemData: Data?
+    @State private var selectedImage: Image?
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let selectedImage {
+                selectedImage
+                    .resizable()
+                    .frame(width: 300, height: 300)
+            } else {
+                Rectangle()
+                    .foregroundStyle(Color.clear)
+                    .frame(width: 300, height: 300)
+            }
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("사진 촬영하기")
+                }
+                
+                PhotosPicker("앨범에서 선택", selection: $selectedItem)
+            }
+        }
+        .onChange(of: selectedItem) { newItem in
+            Task {
+                if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                    selectedItemData = data
+            
+                    if let uiImage = UIImage(data: data) {
+                        selectedImage = Image(uiImage: uiImage)
+                    }
+                }
+            }
+        }
     }
 }
 
