67c3cb61bcfd29b97c9f5728f5c53a8de5dae0a5
hyunsoo <mm083434@naver.com>
2025-10-25 17:04:50 +0900

    생성 - GeminiAPI 통신을 위한 Service 파일 생성

---
+++ SmartCane/Services/GeminiAPIService.swift
@@ -0,0 +1,10 @@
+/*
+ Model - Gemini 2.5 Flash
+ Created - 10/25 2025
+ 
+ 스마트 지팡이 사물 인식을 위한 서비스
+    - 신호등 인식 (우선순위)
+    - 사물 스캔 후 사용자에게 알림
+*/
+
+import Foundation
--- SmartCane/Services/VisionAPIService.swift
+++
@@ -1,3 +0,0 @@
-// OpenAI Vision API 테스트를 위한 임시 파일
-
-import Foundation
