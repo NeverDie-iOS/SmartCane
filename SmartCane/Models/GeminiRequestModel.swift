3477ec8494bf83b63a5fec959de01cdfb4a2d31f
hyunsoo <mm083434@naver.com>
2025-10-25 17:54:29 +0900

    구현 - Gemini 요청 모델 구현

--- SmartCane/Models/GeminiRequestModel.swift
+++ SmartCane/Models/GeminiRequestModel.swift
@@ -1,1 +1,39 @@
+/*
+ 사물 인식 기능을 위한 Gemini- 2.5Flash 모델의 API요청 모델
+*/
+
 import Foundation
+
+// MARK: - 1
+struct GeminiRequest: Encodable {
+    let contents: [Content]
+    let generationConfig: GenerationConfig?
+}
+
+// MARK: - 2
+struct Content: Encodable {
+    let parts: [Part]
+}
+
+// MARK: - 3
+struct Part: Encodable {
+    let text: String?
+    let inlineData: InlineData?
+}
+
+// MARK: - 4
+struct InlineData: Encodable {
+    let mimeType: String    // Ex) "image/jpeg"
+    let data: String
+}
+
+// MARK: - 5
+struct GenerationConfig: Encodable {
+    let temperature: Double?
+    let thinkingConfig: ThinkingConfig?
+}
+
+// MARK: - 6
+struct ThinkingConfig: Encodable {
+    let thinkingBudget: Int?
+}
---
+++ SmartCane/Models/GeminiResponseModel.swift
@@ -0,0 +1,5 @@
+/*
+ 사물 인식 기능을 위한 Gemini- 2.5Flash 모델의 API응답 모델
+*/
+
+import Foundation
