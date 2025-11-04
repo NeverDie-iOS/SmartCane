94be525f7441e4d68c4f7b9f0d4b7edbda9692e2
hyunsoo <mm083434@naver.com>
2025-11-04 14:39:48 +0900

    생성 - Sirikit 테스트를 위한 뷰 생성

---
+++ Guide/Views/SiriKitTestView.swift
@@ -0,0 +1,18 @@
+//
+//  SiriKitTestView.swift
+//  Guide
+//
+//  Created by 현수 on 11/4/25.
+//
+
+import SwiftUI
+
+struct SiriKitTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SiriKitTestView()
+}
