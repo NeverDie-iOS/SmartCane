bc2a0c8bffbe2ad99fd08deab503fa3396ebf64b
hyunsoo <mm083434@naver.com>
2025-11-21 02:24:07 +0900

    기능 구현 - YOLO 모델 탑재 및 문 인식 기능 구현(현재 아이폰 카메라로 기능)

---
+++ Guide/Views/DoorDetectionView.swift
@@ -0,0 +1,18 @@
+//
+//  DoorDetectionView.swift
+//  Guide
+//
+//  Created by 현수 on 11/7/25.
+//
+
+import SwiftUI
+
+struct DoorDetectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoorDetectionView()
+}
